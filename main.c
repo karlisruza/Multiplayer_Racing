e294ad7efd3534ebb66942d5fd77003b2f7a99d8
kassfir <kasparseglitisriga@gmail.com>
2020-04-11 15:19:39 +0300

    commented main, and changed the order of the functions so it would be easier to parse for humans.

--- main.c
+++ main.c
@@ -5,32 +5,79 @@
 #include <math.h>
 #include "car/car.h"
 
-const int height = 40; //y axis
-const int width = 150; //x axis
+const int height = 40; //height of the map
+const int width = 150; //width of the map
 
-// struct coords{
-// 	int x;
-// 	int y;
-// };
-// struct car{
-// 	int playerId;
-// 	struct coords* head;
-// 	struct coords* mid;
-// 	struct coords* tail;
-// 	int angle;
-// };
-// void initCar(struct car** player){
-// 	(*player)->head = malloc(sizeof(struct coords));
-// 	(*player)->mid = malloc(sizeof(struct coords));
-// 	(*player)->tail = malloc(sizeof(struct coords));
-// 	return;
-// }
+	//provides forward motion when the forward motion key is pressed
+void moveCar(struct car** player, WINDOW* win, bool forward);
+
+	//rotates car in ccw or cw direction when, respectively, A or D are pressed
+void rotateCar(struct car** player, WINDOW* win, bool clockwise);
+
+	//determines which key is pressed at a certain moment
+void keyPress(struct car** player, WINDOW* win);
+
+	//Places symbols after each completion of the moveCar function and rotateCar functions
+void drawCar(WINDOW* win, struct car** player);
+
+	//	 a map with x's before spawning the car
+void drawMap(WINDOW * win);
+
+	//Updates the location of the car in the Terminal
+void printCoords(struct car** player, WINDOW* win);
+
+
+int main(void){
+		//necessary to transform terminal into a map
+	initscr();
+	noecho();
+
+		//creates a rectangular border for the map.
+	WINDOW * win = newwin(height, width, 0, 0);
+	wborder(win, 0, 0, 0, 0, 0, 0, 0, 0);
+	refresh();
+
+		//creates the track
+	drawMap(win);
+	wrefresh(win);
+
+		//creates the car object.
+	struct car* player1 = malloc(sizeof(struct car));
+	initCar(&player1);
+	player1->angle=0;
+	player1->head->x = 75;
+	player1->head->y = 31;
+	player1->mid->x = 74;
+	player1->mid->y = 31;
+	player1->tail->x = 73;
+	player1->tail->y = 31;
+
+		//car is placed according to the coordinates shown in the car object. 
+	drawCar(win, &player1);
+	wrefresh(win);
+	refresh();
+
+		//if a key is pressed, calls functions that move the car and read the pressed buttons.
+	while(true){
+		keyPress(&player1, win);
+		// printCoords(&player1, win);
+		drawCar(win, &player1);
+		wrefresh(win);
+		refresh();
+	}
+
+	endwin();
+	return 0;
+}
+
+
 void moveCar(struct car** player, WINDOW* win, bool forward){
 	int hx = (*player)->head->x;
 	int hy = (*player)->head->y;
 	int tx = (*player)->tail->x;
 	int ty = (*player)->tail->y;
 
+		//depending on the rotarion of the car in steps of 45 degrees, the coordinates are increased in the direction.
     switch((*player)->angle){
 	  	case 0: //0
 	  		if(forward){
@@ -105,6 +152,8 @@ void moveCar(struct car** player, WINDOW* win, bool forward){
 	  		}
    			break;
 	}
+
+		//if w is pressed, check is the block in front is an obstacle. Move forward, if safe
 	if(forward){
 		if(mvwinch(win, hy, hx) == ' '){
 			mvwprintw(win, (*player)->tail->y, (*player)->tail->x, " ");
@@ -117,6 +166,8 @@ void moveCar(struct car** player, WINDOW* win, bool forward){
 		}
 		return;
 	}
+
+		//if w is pressed, check is the block behind is an obstacle. Move backward, if safe.
 	else{
 		if(mvwinch(win, ty, tx) == ' '){
 			mvwprintw(win, (*player)->head->y, (*player)->head->x, " ");
@@ -130,7 +181,10 @@ void moveCar(struct car** player, WINDOW* win, bool forward){
 		return;		
 	}
 }
+
 void rotateCar(struct car** player, WINDOW* win, bool clockwise){
+		//checks if D is pressed on the keyboard. If true, rotates the car clockwise. 
+		//Else, it is considered A was pressed, and will turn counter-clockwise.
 	if(clockwise){
 		(*player)->angle +=1;
 		if((*player)->angle == 8){
@@ -222,10 +276,6 @@ void rotateCar(struct car** player, WINDOW* win, bool clockwise){
 	}
 	return;
 }
-	//1 = 0*; 2 = 45*; 3 = 90*; 4 = 135*; 5 = 180*; 6 = 225*; 7 = 270*; 8 = 315*
-	// ==0		  0			0	    0		0==		     =		    =        =		
-	//			 =			=	     =                  =		    =         =
-	//			=			=         =                0		    0          0
 
 void keyPress(struct car** player, WINDOW* win){
 	int key = getch();
@@ -241,6 +291,7 @@ void keyPress(struct car** player, WINDOW* win){
 	    case 'a':
 	    	rotateCar(&(*player), win, false);
 	     	break;
+
 	    case 'd':
 	    	rotateCar(&(*player), win, true);
 	     	break;
@@ -253,6 +304,11 @@ void drawCar(WINDOW* win, struct car** player){
 		mvwprintw(win, (*player)->mid->y, (*player)->mid->x, "=");
 		mvwprintw(win, (*player)->tail->y, (*player)->tail->x, "=");
 }
+	//1 = 0*; 2 = 45*; 3 = 90*; 4 = 135*; 5 = 180*; 6 = 225*; 7 = 270*; 8 = 315*
+	// ==0		  0			0	    0		0==		     =		    =        =		
+	//			 =			=	     =                  =		    =         =
+	//			=			=         =                0		    0          0
+
 
 void drawMap(WINDOW * win){
 	double realx, realy;
@@ -278,48 +334,10 @@ void drawMap(WINDOW * win){
 	}
 	return;
 }
+
 void printCoords(struct car** player, WINDOW* win){
 	mvwprintw(win, 6, 6, "head x: %d, head y: %d\n mid x: %d, mid y: %d\n tail x: %d tail y: %d\n",
 		(*player)->head->x, (*player)->head->y, (*player)->mid->x, (*player)->mid->y, (*player)->tail->x, (*player)->tail->y
 	);
 	return;
-}
-
-int main(void){
-	initscr();
-	noecho();
-	
-	WINDOW * win = newwin(height, width, 0, 0);
-	wborder(win, 0, 0, 0, 0, 0, 0, 0, 0);
-	refresh();
-
-	drawMap(win);
-	wrefresh(win);
-
-	struct car* player1 = malloc(sizeof(struct car));
-	initCar(&player1);
-	player1->angle=0;
-	player1->head->x = 75;
-	player1->head->y = 31;
-	player1->mid->x = 74;
-	player1->mid->y = 31;
-	player1->tail->x = 73;
-	player1->tail->y = 31;
-
-	drawCar(win, &player1);
-	wrefresh(win);
-	refresh();
-
-	while(true){
-		keyPress(&player1, win);
-		// printCoords(&player1, win);
-		drawCar(win, &player1);
-		wrefresh(win);
-		refresh();
-	}
-
-	endwin();
-
-
-	return 0;
 }
