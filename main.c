4d7d9165347388ea98b984d18e48bbbf4689dfb9
kassfir <kasparseglitisriga@gmail.com>
2020-04-15 14:53:23 +0300

    minor improvements in comments

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 #include <math.h>
 #include "car/car.h"
 
-//TODO - have a press anykey to continue for the screen size thing. 
-	//
 
 #define MAP_COLOR     		COLOR_PAIR(1)
 #define PLAYER_ONE_COLOR    COLOR_PAIR(2)
 #define PLAYER_TWO_COLOR    COLOR_PAIR(3)
 
-const int height = 40; //height of the map
-const int width = 150; //width of the map
+const int mapHeight = 40;
+const int mapWidth = 150;
+
+// CONTROLS FUNCTIONS DECLARATIONS
 
 	//provides forward motion when the forward motion key is pressed
 void moveCar(struct car** player, WINDOW* win, bool forward);
@@ -21,8 +21,14 @@ void moveCar(struct car** player, WINDOW* win, bool forward);
 	//rotates car in ccw or cw direction when, respectively, A or D are pressed
 void rotateCar(struct car** player, WINDOW* win, bool clockwise);
 
+	//since fields due to colour in curses are not simply chars anymore,
+		//this function checks if the selected coordinates contain a ' ' or '|'
+bool checkMove(WINDOW* win, int y, int x);
+
 	//determines which key is pressed at a certain moment
 void keyPress(struct car** player, WINDOW* win);
+
+// GRAPHICS FUNCTIONS DECLARATIONS
 
 	//Places symbols after each completion of the moveCar function and rotateCar functions
 void drawCar(WINDOW* win, struct car** player);
@@ -33,27 +39,21 @@ void drawMap(WINDOW * win);
 	//draws finish line and renews it when cars pass.
 void drawFinishLine (WINDOW * win);
 	
+// UI FUNCTIONS DECLARATIONS
 	//outputs stats such as player color, lap count, etc. 
 void outputPlayerStats (struct car** player, WINDOW* win);
 
-	//waits for player to resize the window if it is too small for the game. 
-void winSizeCheck (WINDOW* win);
-
-	//since fields due to colour in curses are not simply chars anymore,
-		//this function checks if the selected coordinates contain a ' ' or '|'
-bool checkMove(WINDOW* win, int y, int x);
-
-	//Once the game is over, output this.
+	//Once the game is over, output this. Basically, congratulates the winner.
 void endScreen (WINDOW* win, struct car** player);
 
-
+	//waits for player to resize the window if it is too small for the game. 
+void winSizeCheck (WINDOW* win);
 int main(void){
 	initscr();
 	noecho();
-	
 
-    	//10 height lines added for outputting the interface. 
-	WINDOW * win = newwin(height + 10, width, 0, 0);
+    	//10 mapHeight lines added for outputting the player information. 
+	WINDOW * win = newwin(mapHeight + 10, mapWidth, 0, 0);
 
 	winSizeCheck(win);
 
@@ -129,13 +129,18 @@ int main(void){
 }
 
 
+//######################################
+//		   CONTROLS FUNCTIONS 
+//######################################
+
 void moveCar(struct car** player, WINDOW* win, bool forward){
 	int hx = (*player)->head->x;
 	int hy = (*player)->head->y;
 	int tx = (*player)->tail->x;
 	int ty = (*player)->tail->y;
 
-		//depending on the rotarion of the car in steps of 45 degrees, the coordinates are increased in the direction.
+		//depending on the rotation of the car in steps of 45 degrees, 
+			//the coordinates are increased in the direction.
     switch((*player)->angle){
 	  	case 0: //0
 	  		if(forward){
@@ -244,7 +249,7 @@ void moveCar(struct car** player, WINDOW* win, bool forward){
 		(*player)->midMark = true;
 	}	
 		//then checks if is on the lower side of the loop, and if they have passed the mid-point
-	else if ((*player)->mid->x == 74 && (*player)->mid->y >= height - 10 && (*player)->midMark == true){
+	else if ((*player)->mid->x == 74 && (*player)->mid->y >= mapHeight - 10 && (*player)->midMark == true){
 		(*player)->laps++;
 		(*player)->midMark = false;		
 	}
@@ -346,6 +351,22 @@ void rotateCar(struct car** player, WINDOW* win, bool clockwise){
 	return;
 }
 
+
+	//A_CHARTEXT lets you parse the character 
+		//all while ignoring colours
+		//This is also frequently used, so for humans it's 
+		//easier to parse this way. 
+bool checkMove(WINDOW * win, int y, int x){
+
+	if (((mvinch(y, x) & A_CHARTEXT) == ' ') || 
+		((mvinch(y, x) & A_CHARTEXT) == '|')){
+			return true;
+	}
+	else{
+		return false;
+	}
+}
+
 void keyPress(struct car** player, WINDOW* win){
 	int key = getch();
 	switch (key)
@@ -368,6 +389,10 @@ void keyPress(struct car** player, WINDOW* win){
 	}
 }
 
+
+//######################################
+//        	GRAPHICS FUNCTIONS 
+//######################################
 
 void drawCar(WINDOW* win, struct car** player){
 
@@ -393,29 +418,29 @@ void drawMap(WINDOW * win){
 	double realx, realy;
 
 	wattron(win, A_DIM);
-
-	for (int i = 1; i < width-1; i++){
+		//creates the upper and lower bound.
+	for (int i = 1; i < mapWidth-1; i++){
 		mvwhline(win, 1, i, 'x', 1);
-		mvwhline(win, height-1, i, 'x', 1);
+		mvwhline(win, mapHeight-1, i, 'x', 1);
 	}
+		//creates the loop of the map
+	for(int y = 2; y < mapHeight-1;y++){
+		for(int x = 1; x < mapWidth-1; x++){
+			realx = x - mapWidth/2;
+			realy = (y - mapHeight/2)*3;
 
-	for(int y = 2; y < height-1;y++){
-		for(int x = 1; x < width-1; x++){
-			realx = x - width/2;
-			realy = (y - height/2)*3;
-
-			if((realx)*(realx) + (realy)*(realy) > (width/2) * (width/2)){
+			if((realx)*(realx) + (realy)*(realy) > (mapWidth/2) * (mapWidth/2)){
 				mvwprintw(win, y, x, "x");
 			}
 		}
 	}
+		//creates the middle of the loop
+	for(int y = 10; y < mapHeight-10;y++){
+		for(int x = 5; x < mapWidth- 8; x++){
+			realx = x - mapWidth/2;
+			realy = (y - mapHeight/2)*3;
 
-	for(int y = 10; y < height-10;y++){
-		for(int x = 5; x < width- 8; x++){
-			realx = x - width/2;
-			realy = (y - height/2)*3;
-
-			if((realx)*(realx) + (realy)*(realy) < ((height)/2) * (height/2) * 6){
+			if((realx)*(realx) + (realy)*(realy) < ((mapHeight)/2) * (mapHeight/2) * 6){
 				mvwprintw(win, y, x, "x");
 			}
 		}
@@ -427,11 +452,10 @@ void drawMap(WINDOW * win){
 }
 
 void drawFinishLine (WINDOW * win){
-			// draws finish line back - should move to the drawcar function so that it doesn't glitch
-			//(this version might fail if there is a thread where the other car passes the finish line)
-		wattron(win, A_DIM);
-		for (int i = height-10; i < height-1; i++){
-				//should check if car is on the line with an if and the coordinates of the car or any car symbols
+			//pretty much just creates a line of "|" symbols at the
+				// starting area of the car
+ 		wattron(win, A_DIM);
+		for (int i = mapHeight-10; i < mapHeight-1; i++){
 			mvwprintw (win, i, 74, "|");
 		}
 		wattroff(win, A_DIM);
@@ -439,33 +463,49 @@ void drawFinishLine (WINDOW * win){
 		return;
 }
 
+//######################################
+//           UI FUNCTIONS 
+//######################################
+
 void outputPlayerStats (struct car** player, WINDOW* win){
 
-	mvwprintw(win, height + 2, 10, 
+	wattron(win, A_BOLD);
+	wattron(win, PLAYER_ONE_COLOR);
+	mvwprintw(win, mapHeight + 2, 10, 
+		"Player one:");
+	wattroff(win, PLAYER_ONE_COLOR);
+	mvwprintw(win, mapHeight + 4, 10, 
 		"Lap count: %d", (*player)->laps);
+	wattroff(win, A_BOLD);
 	
 	return;
 
 }
 
+
 void winSizeCheck (WINDOW* win){
-	while (COLS < width || LINES < height+10){
+	while (COLS < mapWidth || LINES < mapHeight+10){
+
+		//basically, before the game begins, it checks it the 
+			// race track fits on the screen and won't start the 
+			// game until everything is order. It's to prevent ncurses glitches. 
 	
 		mvwprintw(win, 1, 3,
 			"You need to increase the window size.\nTry decreasing character size (ctrl + [-])\nor increasing the terminal window size.");
 
-		if (COLS < width){
+			//outputs if the terminal width is not enough
+		if (COLS < mapWidth){
 			mvwprintw(win, 4, 3, 
-				"Window width should increase to %d. Increase it by %d.", width, width - COLS);
+				"Window width should increase to %d. Increase it by %d.", mapWidth, mapWidth - COLS);
 		}
 		else {
 			move(4, 3);
-			clrtoeol();
+			clrtoeol();	//this removes all chars from the cursor
 		}
-		
-		if (LINES < height+10){
+			//outputs if the terminal height is not enough
+		if (LINES < mapHeight+10){
 			mvwprintw(win, 5, 3, 
-				"Window height should increase to %d. Increase it by %d.", height, height - LINES+10);
+				"Window height should increase to %d. Increase it by %d.", mapHeight + 10, mapHeight - LINES+10);
 		}
 		else {
 			move(5, 3);
@@ -473,33 +513,22 @@ void winSizeCheck (WINDOW* win){
 		}
 		
 		mvwprintw(win, 7, 1, 
-			"Press any key to check again.");
+			"Press [enter] to check again.");
 
 		refresh();
 		wrefresh(win);
 
-		getchar();
+			//loop for pressing enter
+		char enter = 0;
+		while (enter != '\r' && enter != '\n') {
+	 		enter = getchar(); 
+	 	};
 
 	}
 
 	werase(win);
 
 	return;
-}
-
-	//A_CHARTEXT lets you parse the character 
-		//all while ignoring colours
-		//This is also frequently used, so for humans it's 
-		//easier to parse this way. 
-bool checkMove(WINDOW * win, int y, int x){
-
-	if (((mvinch(y, x) & A_CHARTEXT) == ' ') || 
-		((mvinch(y, x) & A_CHARTEXT) == '|')){
-			return true;
-	}
-	else{
-		return false;
-	}
 }
 
 void endScreen(WINDOW* win, struct car** player){
