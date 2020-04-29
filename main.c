fe6019f783a0ab4cb80c2b566a7ecd607015e5bf
kassfir <kasparseglitisriga@gmail.com>
2020-04-29 22:28:22 +0300

    jeeebaaal

--- main.c
+++ main.c
@@ -7,12 +7,33 @@
 #include <math.h>
 
 
+/*
+[enter]
+keycode  28 press
+keycode  28 release
+
+w
+keycode  17 press
+keycode  17 release
+
+a
+keycode  30 press
+keycode  30 release
+
+s
+keycode  31 press
+keycode  31 release
+
+d
+keycode  32 press
+keycode  32 release
+*/
+
 #define MAP_COLOR     		COLOR_PAIR(1)
 #define PLAYER_ONE_COLOR    COLOR_PAIR(2)
 #define PLAYER_TWO_COLOR    COLOR_PAIR(3)
 #define fps 				10
 #define accel				3
-#define pi					3
 #define plRad				(*player)->angle * M_PI
 
 const int mapHeight = 40;
@@ -184,19 +205,12 @@ void rotateCar(struct car** player, WINDOW* win, bool clockwise){
 		//checks if D is pressed on the keyboard. If true, rotates the car clockwise. 
 		//otherwise, it is considered A was pressed, and will turn counter-clockwise.
 	if(clockwise){
-		(*player)-> angle += 0.2;
-		if((*player)->angle >= 2 * M_PI){
-	   		(*player)->angle = 0;
-	    }
+		(*player)-> angle += 0.15;
 	}
 	else{
-		(*player)->angle -= -0.2;
-		if((*player)->angle <= 0.2){
-	   		(*player)->angle = 2 * M_PI;
-	    }
+		(*player)->angle -= 0.15;
 	}
-	mvwprintw(win, 2, 2, "%d", (*player)->angle);
-
+	/*
 	double hx = ((*player)->mid->x) + cos((*player)->angle * M_PI);
 	double hy = ((*player)->mid->y) + sin((*player)->angle * M_PI);
 	double tx = ((*player)->mid->x) - cos((*player)->angle * M_PI);
@@ -210,7 +224,7 @@ void rotateCar(struct car** player, WINDOW* win, bool clockwise){
 		//(*player)->angle += 0.1;
 		return;
 	}
-/*
+
 	//if previous if fails, changes angle back to previous, because rotation was not possible
 	if(clockwise){
 		(*player)->angle -=1;
@@ -246,27 +260,7 @@ bool checkMove(WINDOW * win, double y, double x){
 	}
 }
 
-void keyPress(struct car** player, WINDOW* win){
-	int key = getch();
-	switch (key)
-	{
-	    case 'w':
-			moveCar(&(*player), win, true);
-	     	break;
 
-	    case 's':
-	    	moveCar(&(*player), win, false);
-	    	break;
-
-	    case 'a':
-	    	rotateCar(&(*player), win, false);
-	     	break;
-
-	    case 'd':
-	    	rotateCar(&(*player), win, true);
-	     	break;
-	}
-}
 
 
 //######################################
@@ -281,9 +275,11 @@ void drawCar(WINDOW* win, struct car** player){
 
 	wattron(win, A_BOLD);
 	wattron(win, PLAYER_ONE_COLOR);
-		mvwprintw(win, round((*player)->mid->y) + sin((*player)->angle), round((*player)->mid->x) + cos((*player)->angle), "0");
+
+		mvwprintw(win, round((*player)->mid->y) - sin((*player)->angle*M_PI), round((*player)->mid->x) - cos((*player)->angle*M_PI), "=");
 		mvwprintw(win, round((*player)->mid->y), round((*player)->mid->x), "=");
-		mvwprintw(win, round((*player)->mid->y) - sin((*player)->angle), round((*player)->mid->x) - cos((*player)->angle), "=");
+		mvwprintw(win, round((*player)->mid->y) + sin((*player)->angle*M_PI), round((*player)->mid->x) + cos((*player)->angle*M_PI), "0");
+
 	wattroff(win, PLAYER_ONE_COLOR);
 	wattroff(win, A_BOLD);
 
@@ -343,6 +339,24 @@ void drawFinishLine (WINDOW * win){
 		return;
 }
 
+/*void drawLine(WINDOW* win, struct coords lineStart, struct coords lineEnd){
+	int yStart, xStart;
+
+	yStart = (lineStart->y > lineEnd->y) ? round(lineEnd->y) : round(lineStart->y);
+	xStart = (lineStart->x > lineEnd->x) ? round(lineEnd->x) : round(lineStart->x);
+
+//		int hline(chtype ch, int n);
+//   	int whline(WINDOW *win, chtype ch, int n);
+
+
+	//int mvwhline(WINDOW *win, int y, int x, chtype ch, int n);
+	int mvwhline(win, int y, int x, chtype ch, int n);
+
+	int mvwvline(WINDOW *win, int y, int x, chtype ch, int n);
+	return;
+
+}*/
+
 //######################################
 //           UI FUNCTIONS 
 //######################################
@@ -361,7 +375,7 @@ void outputPlayerStats (struct car** player, WINDOW* win){
 		mvwprintw(win, mapHeight + 6, 10, 
 			"Angle: %f", (*player)->angle * M_PI);
 		mvwprintw(win, mapHeight + 7, 10, 
-			"X axis: %f", (*player)->mid->y);
+			"X axis: %f", (*player)->mid->x);
 		mvwprintw(win, mapHeight + 8, 10, 
 			"Y axis: %f", (*player)->mid->y);
 		
@@ -454,3 +468,204 @@ void endScreen(WINDOW* win, struct car** player){
 	return;
 
 }
+
+
+void keyPress(struct car** player, WINDOW* win){
+	int key = getch();
+	switch (key)
+	{
+	    case 'w':
+			moveCar(&(*player), win, true);
+	     	break;
+
+	    case 's':
+	    	moveCar(&(*player), win, false);
+	    	break;
+
+	    case 'a':
+	    	rotateCar(&(*player), win, false);
+	     	break;
+
+	    case 'd':
+	    	rotateCar(&(*player), win, true);
+	     	break;
+	}
+}
+
+
+//basically, make a thread that constantly keeps reading keypresses. The code underneath takes from showkey.c and some stuff online.
+
+
+// static void clean_up(void) {
+// 	if (ioctl(fd, KDSKBMODE, oldkbmode)) {
+// 		perror("KDSKBMODE");
+// 		exit(1);
+// 	}
+// 	if (tcsetattr(fd, 0, &old) == -1)
+// 		perror("tcsetattr");
+// 	close(fd);
+// }
+
+
+// static void
+// get_mode(void) {
+//         char *m;
+// 	if (ioctl(fd, KDGKBMODE, &oldkbmode)) {
+// 		perror("KDGKBMODE");
+// 		exit(1);
+// 	}
+// 	switch(oldkbmode) {
+// 	  case K_RAW:
+// 	    m = "RAW"; break;
+// 	  case K_XLATE:
+// 	    m = "XLATE"; break;
+// 	  case K_MEDIUMRAW:
+// 	    m = "MEDIUMRAW"; break;
+// 	  case K_UNICODE:
+// 	    m = "UNICODE"; break;
+// 	  default:
+// 	    m = _("?UNKNOWN?"); break;
+// 	}
+// 	printf(_("kb mode was %s\n"), m);
+// 	if (oldkbmode != K_XLATE) {
+// 	    printf(_("[ if you are trying this under X, it might not work\n"
+// 		     "since the X server is also reading /dev/console ]\n"));
+// 	}
+// 	printf("\n");
+// }
+
+
+// int
+// main (int argc, char *argv[]) {
+// 	const char *short_opts = "haskV";
+// 	const struct option long_opts[] = {
+// 		{ "help",	no_argument, NULL, 'h' },
+// 		{ "ascii",	no_argument, NULL, 'a' },
+// 		{ "scancodes",	no_argument, NULL, 's' },
+// 		{ "keycodes",	no_argument, NULL, 'k' },
+// 		{ "version",	no_argument, NULL, 'V' },
+// 		{ NULL, 0, NULL, 0 }
+// 	};
+// 	int c;
+// 	int show_keycodes = 1;
+// 	int print_ascii = 0;
+// 	struct termios new;
+// 	unsigned char buf[16];
+// 	int i, n;
+// 	set_progname(argv[0]);
+// 	setlocale(LC_ALL, "");
+// 	bindtextdomain(PACKAGE, LOCALEDIR);
+// 	textdomain(PACKAGE);
+// 	while ((c = getopt_long(argc, argv,
+// 		short_opts, long_opts, NULL)) != -1) {
+// 		switch (c) {
+// 			case 's':
+// 				show_keycodes = 0;
+// 				break;
+// 			case 'k':
+// 				show_keycodes = 1;
+// 				break;
+// 			case 'a':
+// 				print_ascii = 1;
+// 				break;
+// 			case 'V':
+// 				print_version_and_exit();
+// 			case 'h':
+// 			case '?':
+// 				usage();
+// 		}
+// 	}
+// 	if (optind < argc)
+// 		usage();
+// 	if (print_ascii) {
+// 		/* no mode and signal and timer stuff - just read stdin */
+// 	        fd = 0;
+// 		if (tcgetattr(fd, &old) == -1)
+// 			perror("tcgetattr");
+// 		if (tcgetattr(fd, &new) == -1)
+// 			perror("tcgetattr");
+// 		new.c_lflag &= ~ (ICANON | ISIG);
+// 		new.c_lflag |= (ECHO | ECHOCTL);
+// 		new.c_iflag = 0;
+// 		new.c_cc[VMIN] = 1;
+// 		new.c_cc[VTIME] = 0;
+// 		if (tcsetattr(fd, TCSAFLUSH, &new) == -1)
+// 			perror("tcgetattr");
+// 		printf(_("\nPress any keys - "
+// 		         "Ctrl-D will terminate this program\n\n"));
+// 		while (1) {
+// 			n = read(fd, buf, 1);
+// 			if (n == 1)
+// 				printf(" \t%3d 0%03o 0x%02x\n",
+// 				       buf[0], buf[0], buf[0]);
+// 			if (n != 1 || buf[0] == 04)
+// 				break;
+// 		}
+// 		if (tcsetattr(fd, 0, &old) == -1)
+// 			perror("tcsetattr");
+// 		exit(0);
+// 	}
+// 	fd = getfd(NULL);
+// 	/* the program terminates when there is no input for 10 secs */
+// 	signal(SIGALRM, watch_dog);
+// 	/*
+// 		if we receive a signal, we want to exit nicely, in
+// 		order not to leave the keyboard in an unusable mode
+// 	*/
+// 	signal(SIGHUP, die);
+// 	signal(SIGINT, die);
+// 	signal(SIGQUIT, die);
+// 	signal(SIGILL, die);
+// 	signal(SIGTRAP, die);
+// 	signal(SIGABRT, die);
+// 	signal(SIGIOT, die);
+// 	signal(SIGFPE, die);
+// 	signal(SIGKILL, die);
+// 	signal(SIGUSR1, die);
+// 	signal(SIGSEGV, die);
+// 	signal(SIGUSR2, die);
+// 	signal(SIGPIPE, die);
+// 	signal(SIGTERM, die);
+// #ifdef SIGSTKFLT
+// 	signal(SIGSTKFLT, die);
+// #endif
+// 	signal(SIGCHLD, die);
+// 	signal(SIGCONT, die);
+// 	signal(SIGSTOP, die);
+// 	signal(SIGTSTP, die);
+// 	signal(SIGTTIN, die);
+// 	signal(SIGTTOU, die);
+// 	get_mode();
+// 	if (tcgetattr(fd, &old) == -1)
+// 		perror("tcgetattr");
+// 	if (tcgetattr(fd, &new) == -1)
+// 		perror("tcgetattr");
+// 	new.c_lflag &= ~ (ICANON | ECHO | ISIG);
+// 	new.c_iflag = 0;
+// 	new.c_cc[VMIN] = sizeof(buf);
+// 	new.c_cc[VTIME] = 1;	/* 0.1 sec intercharacter timeout */
+// 	if (tcsetattr(fd, TCSAFLUSH, &new) == -1)
+// 		perror("tcsetattr");
+// 	if (ioctl(fd, KDSKBMODE, show_keycodes ? K_MEDIUMRAW : K_RAW)) {
+// 		perror("KDSKBMODE");
+// 		exit(1);
+// 	}
+// 	printf(_("press any key (program terminates 10s after last keypress)...\n"));
+// 	while (1) {
+// 		alarm(10);
+// 		n = read(fd, buf, sizeof(buf));
+// 		for (i = 0; i < n; i++) {
+// 			if (!show_keycodes)
+// 				printf("0x%02x ", buf[i]);
+// 			else
+// 				printf(_("keycode %3d %s\n"),
+// 					buf[i] & 0x7f,
+// 					buf[i] & 0x80 ? _("release")
+// 				                      : _("press"));
+// 		}
+// 		if (!show_keycodes)
+// 			printf("\n");
+// 	}
+// 	clean_up();
+// 	exit(0);
+// }
